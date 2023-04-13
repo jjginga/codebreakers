02dc5fc2df8f5fb341685b28da6726f25320adaf
Gabriel Aragao <gabriel.aragao.comp@gmail.com>
2023-04-13 09:20:31 +0100

    implement composed delimiters and fix empty value

--- src/breakers/code/Tokenizer.java
+++ src/breakers/code/Tokenizer.java
@@ -1,11 +1,12 @@
 package breakers.code;
 
-import breakers.code.grammar.tokens.BASIC_GRAMMAR;
-import breakers.code.grammar.tokens.BASIC_SYMBOLS;
-import breakers.code.grammar.tokens.KeyValueToken;
+import breakers.code.grammar.tokens.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import static breakers.code.grammar.tokens.COMPOSED_DELIMITER.EQUALS_EQUALS;
 
 /*This class gets the source code and splits it into tokens
 * it reads the sourceCode char by char and splits it into tokens by a delimiter
@@ -22,6 +23,12 @@ public class Tokenizer {
     private String statmentTerminator = ";";
     private String commentDelimiter = "#";
 
+    private final List<String> composedDelimiters = Arrays.asList("==", "!=", ">=", "<=", "++", "--");
+
+    private StringBuilder currentValue = new StringBuilder();
+    private List<KeyValueToken> currentLine = new ArrayList<KeyValueToken>();
+
+
     /* Constructor */
     public Tokenizer(String sourceCode) {
         this.sourceCode = sourceCode;
@@ -30,11 +37,6 @@ public class Tokenizer {
 
     //TODO: composed delimiters, like "==", "!=", ">=", "<=", "++", "--"
     public List<List<KeyValueToken>> tokenize() {
-
-        StringBuilder currentValue = new StringBuilder();
-        List<KeyValueToken> currentLine = getNewList();
-
-
         //we process the text one char at a time
         for (int i = 0; i < sourceCode.length(); i++) {
 
@@ -56,18 +58,33 @@ public class Tokenizer {
 
             //if the char is a delimiter or statment terminator we have a token and have to add it to the current line
             if(isValueChanger(currentChar)) {
+                KeyValueToken token;
 
                 //add the current value
                 //TODO: not preform this for empty current value
-                KeyValueToken token = getKeyValueToken(currentValue.toString());
-                currentLine.add(token);
-
-                currentValue = new StringBuilder();
+                if(!currentValue.toString().isEmpty()) {
+                    token = getKeyValueToken(null, currentValue.toString());
+                    storeFoundToken(token);
+                }
 
                 //the char is statment terminator or delimiter
                 if(!isToBeIgnored(currentChar)) {
                     //TODO: before creating token we should peek to see if it is composed like "==".
-                    token = getKeyValueToken(String.valueOf(currentChar));
+
+                    // composed delimiter like "==" --> We add the two chars as a single token and move the counter 2 indexes ahead
+                    String composedString = getComposedString(currentChar, i);
+                    if(isComposedDelimiter(composedString)){
+                        //TODO --> Need to add the correct one (for now it's hard coded to be only "EQUALS EQUALS" we need the other composed delimiters as well
+                        token = getKeyValueToken(EQUALS_EQUALS, composedString);
+                        currentLine.add(token);
+
+                        // Move
+                        i++;
+                        continue;
+                    }
+
+                    // TODO --> Assign correct key according to grammar
+                    token = getKeyValueToken(null, String.valueOf(currentChar));
                     currentLine.add(token);
                 }
 
@@ -92,8 +109,9 @@ public class Tokenizer {
     }
 
 
-    private static KeyValueToken getKeyValueToken(String currentValue) {
-        return new KeyValueToken(null, currentValue);
+    private static KeyValueToken getKeyValueToken(BASIC_GRAMMAR grammarKey, String currentValue) {
+        return new KeyValueToken(grammarKey, currentValue); // @Joel --> Joel, why was this being set to null? We need to keep track of the types of token we have for later validations
+        // For instance we cannot have 2 BASIC_VARs in a row like "int int", to validate that we need to know their types --> which will be the KEY on the KeyValueToken
     }
 
 
@@ -129,8 +147,37 @@ public class Tokenizer {
         return isDelimiter(c) || isStatementTerminator(c) || isToBeIgnored(c);
     }
 
+    private boolean isComposedDelimiter (String composedString) {
+        if (composedDelimiters.contains(composedString)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private String getComposedString (char c, int currentIndex) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(c);
+        sb.append(sourceCode.charAt(currentIndex + 1));
+
+        String composedString = sb.toString();
+
+        return composedString;
+    }
+
     private boolean isCommentDelimiter(char c) {
         return commentDelimiter.equals(String.valueOf(c));
+    }
+
+    /*
+    * Once a delimiter or a statement terminator is found we store the "cached token" into the read values
+    * Meaning that the token that was being cached is now finished and a new one is supposed to be read
+    * */
+    private void storeFoundToken(KeyValueToken token) {
+        // TODO --> Assign correct key according to grammar
+        currentLine.add(token);
+
+        currentValue = new StringBuilder();
     }
 
 }
